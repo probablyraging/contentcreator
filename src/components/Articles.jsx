7c469f30cb84638144228966c12c5c8b4d0ee5d7
ProbablyRaging <probablyraging@gmail.com>
2023-06-01 03:56:58 +1000

    Loading skeleton for resources

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,21 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text } from "@nextui-org/react";
 import { Link } from 'react-router-dom';
-import { Loader, BgGradients } from '../components';
-import { useFetchResources } from '../constants/utils';
+import { BgGradients } from '../components';
 import slugify from 'slugify';
 import styles from "../style";
+import axios from 'axios';
 
 const Articles = ({ darkMode }) => {
-    const resources = useFetchResources();
+    const [isLoading, setIsLoading] = useState(true);
+    const [resources, setResources] = useState([]);
 
-    if (!resources) {
-        return <Loader darkMode={darkMode} />
-    }
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await axios.post('https://creatordiscord.xyz/api/resources');
+            setResources(response.data.message);
+            setIsLoading(false);
+        };
+        fetchData();
+    }, []);
 
     const transformTitleToId = (title) => {
         return slugify(title, { lower: true, strict: true });
     };
+
+    if (isLoading) {
+        return (
+            <section id="home" className={`flex flex-row sm:flex-col pt-6 pb-16 xs:pb-5`}>
+                <div className={`flex-1 ${styles.flexStart} flex-col px-16 sm:px-6 xxlup:px-0 sm:items-center`}>
+                    <BgGradients />
+                    <div className='flex justify-center flex-wrap gap-10'>
+                        {Array.from({ length: 6 }, (_, index) => (
+                            <div
+                                key={index}
+                                className={`${darkMode ? 'bg-[#16181A] drop-shadow-cardShadowSmDark' : 'bg-[#fff] drop-shadow-cardShadowSm'} relative flex flex-col min-h-[300px] max-w-[400px] md:max-w-full md:min-h-[250px] rounded-2xl border-none card-anim min-w-[400px] md:min-w-full`}
+                            >
+                                <div className={`h-36 ${darkMode ? 'bg-gray-700' : 'bg-gray-200'} rounded-2xl rounded-b-none`}></div>
+                                <div className="p-4">
+                                    <div
+                                        style={{ width: Math.floor(Math.random() * (100 - 50 + 1)) + 50 + '%' }}
+                                        className={`h-4 ${darkMode ? 'bg-gray-700' : 'bg-gray-200'} rounded mb-6 animate-pulse`}>
+                                    </div>
+                                    <div
+                                        style={{ width: Math.floor(Math.random() * (80 - 70 + 1)) + 70 + '%' }}
+                                        className={`h-2 ${darkMode ? 'bg-gray-700' : 'bg-gray-200'} rounded mt-4 animate-pulse`}>
+                                    </div>
+                                    <div
+                                        style={{ width: Math.floor(Math.random() * (60 - 40 + 1)) + 40 + '%' }}
+                                        className={`h-2 ${darkMode ? 'bg-gray-700' : 'bg-gray-200'} rounded mt-4 animate-pulse`}>
+                                    </div>
+                                    <div
+                                        style={{ width: Math.floor(Math.random() * (90 - 60 + 1)) + 60 + '%' }}
+                                        className={`h-2 ${darkMode ? 'bg-gray-700' : 'bg-gray-200'} rounded mt-4 animate-pulse`}>
+                                    </div>
+                                    <div
+                                        style={{ width: Math.floor(Math.random() * (80 - 30 + 1)) + 30 + '%' }}
+                                        className={`h-2 ${darkMode ? 'bg-gray-700' : 'bg-gray-200'} rounded mt-4 animate-pulse`}>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </section>
+        );
+    }
 
     return (
         <section id="home" className={`flex flex-row sm:flex-col pt-6 pb-16 xs:pb-5`}>
@@ -26,7 +74,7 @@ const Articles = ({ darkMode }) => {
                         <Link key={index} to={transformTitleToId(item.title)}>
                             <div className={`${darkMode ? 'bg-[#16181A] drop-shadow-cardShadowSmDark hover:drop-shadow-cardShadowLgDark' : 'bg-[#fff] drop-shadow-cardShadowSm hover:drop-shadow-cardShadowLg'} relative flex flex-col min-w-0 min-h-[300px] max-w-[400px] md:max-w-full md:min-h-[250px] rounded-2xl border-none hover:translate-y-[-2px] card-anim`}>
                                 <img src={item.thumb} className="h-36 object-cover rounded-2xl rounded-b-none" alt={item.title} />
-                                <div className=" p-4">
+                                <div className="p-4">
                                     <Text>
                                         <Link to={transformTitleToId(item.title)} className='font-semibold'>
                                             {item.title}
--- src/components/partials/FeedbackCard.jsx
+++ src/components/partials/FeedbackCard.jsx
@@ -2,43 +2,32 @@ import React from 'react';
 import { Text } from "@nextui-org/react";
 import { Tilt } from 'react-tilt';
 import { quotes } from "../../assets";
-import { motion } from 'framer-motion';
 
 const FeedbackCard = ({ darkMode, content, name, title, img }) => (
     <Tilt options={{ max: 15, scale: 1 }}>
-        <motion.div
-            initial={{ y: 100 }}
-            whileInView={{ y: 0 }}
-            transition={{
-                type: "spring",
-                stiffness: 400,
-                damping: 18,
-                delay: 0,
-            }}>
-            <div className={`${darkMode ? 'bg-[#16181A] drop-shadow-cardShadowSmDark hover:drop-shadow-cardShadowLgDark' : 'bg-[#fff] drop-shadow-cardShadowSm hover:drop-shadow-cardShadowLg'} relative flex flex-col min-w-0 min-h-[300px] max-w-[400px] md:w-full md:min-h-[250px] rounded-2xl border-none card-anim`}>
-                <div className="p-4">
-                    <img src={quotes} alt="double_quotes" className="w-[42.6px] h-[27.6px] object-contain sm:max-w-[32px]" />
-                    <p className="font-poppins font-normal text-[18px] leading-[32.4px] my-10 sm:text-[14px] sm:text-center">
-                        {content}
-                    </p>
-                </div>
-                <div>
-                    <div className={`${darkMode ? 'bg-[#1b1e20]' : 'bg-[#f4f4f4]'} flex flex-row py-2 px-4 rounded-2xl rounded-t-none`}>
-                        <img src={img} alt={name} className="w-[48px] h-[48px] rounded-full mr-6" />
-                        <div>
-                            <Text h4
-                                className="font-poppins font-semibold text-[20px] sm:text-[18px] leading-[32px]"
-                                css={{ display: 'inline-block', textGradient: "90deg, rgba(14,98,194,1) 0%, rgba(83,155,237,1) 76%, rgba(14,98,194,1) 100%", }}>
-                                {name}
-                            </Text>
-                            <p className="font-poppins font-normal text-[16px] leading-[24px]">
-                                {title}
-                            </p>
-                        </div>
+        <div className={`${darkMode ? 'bg-[#16181A] drop-shadow-cardShadowSmDark hover:drop-shadow-cardShadowLgDark' : 'bg-[#fff] drop-shadow-cardShadowSm hover:drop-shadow-cardShadowLg'} relative flex flex-col min-w-0 min-h-[300px] max-w-[400px] md:w-full md:min-h-[250px] rounded-2xl border-none card-anim`}>
+            <div className="p-4">
+                <img src={quotes} alt="double_quotes" className="w-[42.6px] h-[27.6px] object-contain sm:max-w-[32px]" />
+                <p className="font-poppins font-normal text-[18px] leading-[32.4px] my-10 sm:text-[14px] sm:text-center">
+                    {content}
+                </p>
+            </div>
+            <div>
+                <div className={`${darkMode ? 'bg-[#1b1e20]' : 'bg-[#f4f4f4]'} flex flex-row py-2 px-4 rounded-2xl rounded-t-none`}>
+                    <img src={img} alt={name} className="w-[48px] h-[48px] rounded-full mr-6" />
+                    <div>
+                        <Text h4
+                            className="font-poppins font-semibold text-[20px] sm:text-[18px] leading-[32px]"
+                            css={{ display: 'inline-block', textGradient: "90deg, rgba(14,98,194,1) 0%, rgba(83,155,237,1) 76%, rgba(14,98,194,1) 100%", }}>
+                            {name}
+                        </Text>
+                        <p className="font-poppins font-normal text-[16px] leading-[24px]">
+                            {title}
+                        </p>
                     </div>
                 </div>
             </div>
-        </motion.div>
+        </div>
     </Tilt>
 );
 
