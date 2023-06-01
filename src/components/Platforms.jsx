67d727e22b10e0c117e20435a1056f030c91951e
ProbablyRaging <probablyraging@gmail.com>
2023-06-01 17:50:53 +1000

    Untracking dist

--- src/components/Platforms.jsx
+++ src/components/Platforms.jsx
@@ -6,7 +6,7 @@ const Platforms = () => (
     <div className={`${styles.flexCenter} flex-wrap w-full my-4`}>
         {platforms.map((platform) => (
             <div key={platform.id} className={`flex-1 ${styles.flexCenter} min-w-[192px] sm:min-w-[0] m-5`}>
-                <img id={platform.id} src={platform.logo} alt={platform.id} className="w-[52px] min-w-[42px] object-contain" />
+                <img id={platform.id} src={platform.logo} alt={platform.id} width="52" height="52" className="w-[52px] min-w-[42px] object-contain" />
             </div>
         ))}
     </div>
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: "/",
+    base: '/',
     plugins: [react()],
+    build: {
+        chunkSizeWarningLimit: 1000,
+    },
 });
